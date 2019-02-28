3aff80f05381ebc1e67f3dfd9b63c967c796a3d9
Diogo <diogo.cardoso@convertr.io>
2019-02-28 14:38:34 +0000

    added users

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
 const PORT = process.env.PORT || 3000;
-const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
-const mongoose = require('mongoose');
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
-const Event = require('./models/event');
+const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-
-app.use('/graphql', graphqlHttp({
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: buildSchema(`
         type Event {
             _id: ID!
@@ -22,11 +25,22 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
         }
     
         type RootQuery {
@@ -35,6 +49,7 @@ app.use('/graphql', graphqlHttp({
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -43,41 +58,84 @@ app.use('/graphql', graphqlHttp({
         }
     `),
     rootValue: {
-        events: ()=> {
-            return Event.find()
-            .then(events => {
-                return events.map(event => {
-                    return { ...event._doc, _id: event.id };
-                })
-            }).catch(err => {
-                throw err
-            })
-        },
-        createEvent: (args)=> {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
             });
-            return event.save().then(result => {
-                console.log(result);
-                return { ...result._doc, _id: event.id };
-            }).catch(err => {
-                console.log(err);
-                throw err;
-            });            
-        }
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          creator: "5c77ef211f7e164c0dd39077"
+        });
+        let createdEvent;
+        return event
+          .save()
+          .then(result => {
+            createdEvent = { ...result._doc, password: null, _id: result.id };
+            return User.findById("5c77ef211f7e164c0dd39077");
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error("User not found.");
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error("User exists already.");
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
+            throw err;
+          });
+      }
     },
     graphiql: true
-}));
+  })
+);
 
-
-mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-lmw36.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
-    , {useNewUrlParser: true}).then(() => {
-        app.listen(PORT);       
-    })
-    .catch((err)=> {
-        console.log(err);
-    });
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-lmw36.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
